d47abf71a3c279027d54db3d276f6f2e5814bb44
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-03-01 10:34:18 +0200

    refactor: update port

--- mobile-app-api/src/main.ts
+++ mobile-app-api/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(8080, '0.0.0.0');
+    await app.listen(3001);
 }
 bootstrap();
 
